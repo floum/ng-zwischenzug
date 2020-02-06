20db1c60744e8807203bf97c795508d882d92d66
efflamm castel <efflamm.castel@gmail.com>
2020-02-06 22:43:59 +0000

    chessboard module & chessground css

---
+++ src/app/chessboard/chessboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chessboard',
+  templateUrl: './chessboard.component.html',
+  styleUrls: ['./chessboard.component.scss']
+})
+export class ChessboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/chessboard/chessboard.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChessboardComponent } from './chessboard.component';
+
+
+
+@NgModule({
+  declarations: [ChessboardComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ChessboardComponent
+  ]
+})
+export class ChessboardModule { }
