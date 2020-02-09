0840bf903e6f13724c3f032ac7faa8ab4ab02373
efflamm castel <efflamm.castel@gmail.com>
2020-02-09 16:44:52 +0000

    register form

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthenticationComponent } from './authentication.component';
 import { AuthenticationService } from '@app/authentication/authentication.service';
+import { RegisterComponent } from '../register/register.component';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [AuthenticationComponent],
+  declarations: [AuthenticationComponent, RegisterComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule
   ],
   providers: [
     AuthenticationService
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
