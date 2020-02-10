158fa38f6900f5e459def9974ef0bd090918ce96
efflamm castel <efflamm.castel@gmail.com>
2020-02-10 17:39:24 +0000

    logout link (not updating)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { UserService } from '@app/user/user.service';
 import { User } from '@app/user/user.model';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -11,13 +13,18 @@ import { User } from '@app/user/user.model';
 export class HeaderComponent implements OnInit {
 
   currentUser: User;
-  
+
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.currentUser = this.userService.currentUserValue;
   }
 
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/login']);
+  }
 }
