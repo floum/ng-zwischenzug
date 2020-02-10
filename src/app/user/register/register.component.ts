5c488d16b0ba104b32fde963067807687c74db5a
efflamm castel <efflamm.castel@gmail.com>
2020-02-10 01:40:56 +0000

    shared module and alert service

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -4,54 +4,56 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
 import { UserService } from '@app/user/user.service';
+import { AlertService } from '@app/shared/alert.service';
 
 @Component({templateUrl: 'register.component.html'})
 export class RegisterComponent implements OnInit {
-    registerForm: FormGroup;
-    loading = false;
-    submitted = false;
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private router: Router,
-        private userService: UserService
-    ) { 
-        // redirect to home if already logged in
-        if (this.userService.currentUserValue) { 
-            this.router.navigate(['/']);
-        }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private alertService: AlertService
+  ) { 
+    // redirect to home if already logged in
+    if (this.userService.currentUserValue) { 
+      this.router.navigate(['/']);
+    }
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
     }
 
-    ngOnInit() {
-        this.registerForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', [Validators.required, Validators.minLength(6)]]
+    this.loading = true;
+    this.userService.register(this.registerForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.alertService.success('Registration successful', true);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
         });
-    }
-
-    // convenience getter for easy access to form fields
-    get f() { return this.registerForm.controls; }
-
-    onSubmit() {
-        this.submitted = true;
-
-        // stop here if form is invalid
-        if (this.registerForm.invalid) {
-            return;
-        }
-
-        this.loading = true;
-        this.userService.register(this.registerForm.value)
-            .pipe(first())
-            .subscribe(
-                data => {
-                  // this.alertService.success('Registration successful', true);
-                    this.router.navigate(['/login']);
-                },
-                error => {
-                  //this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
+  }
 }
 
